5241f4e56916ce63d7ec6c0826ed9c7659993964
bkinasevych <bkinasevych@gmail.com>
2025-10-29 16:50:52 -0500

    update

--- scripts/import-data.R
+++ scripts/import-data.R
@@ -1,4 +1,3 @@
-
 # Import packages ---------------------------------------------------------
 
 library(surveymonkey)
@@ -11,52 +10,51 @@ library(tidyverse)
 options(sm_oauth_token = Sys.getenv("SM_TOKEN"))
 
 # get survey data
-survey_df <- 525018787 |> 
-  fetch_survey_obj() |> 
+survey_df <- 525018787 |>
+  fetch_survey_obj() |>
   parse_survey()
 
 # set new column headers
-col_names <- (
-  c("x_survey_id",
-    "x_collector_id",
-    "id",
-    "date_created",
-    "x_date_modified",
-    "response_status",
-    "x_ip_address",
-    "funded_past_3_yrs",
-    "applied_not_funded",
-    "org_role",
-    "org_role_other_txt",
-    "primary_focus",
-    "primary_focus_other_txt",
-    "postal_code",
-    "purpose",
-    "confident_in_mac_ability",
-    "deadline_comms",
-    "guidelines_comms",
-    "manipogo",
-    "trasparent_granting",
-    "response_enquiries",
-    "response_results",
-    "feedback",
-    "complaints_process",
-    "improve",
-    "connection_to_arts_community",
-    "disconnect_explain_txt",
-    "rank_advocacy",
-    "rank_measure_impact",
-    "rank_board_trg",
-    "rank_financial_trg",
-    "rank_leadership_trg",
-    "rank_shared_services",
-    "rank_loan_access",
-    "rank_networking",
-    "rank_marketing",
-    "future_impact_txt",
-    "comments_txt"
-  )
-)
+col_names <- (c(
+  "x_survey_id",
+  "x_collector_id",
+  "id",
+  "date_created",
+  "x_date_modified",
+  "response_status",
+  "x_ip_address",
+  "funded_past_3_yrs",
+  "applied_not_funded",
+  "org_role",
+  "org_role_other_txt",
+  "primary_focus",
+  "primary_focus_other_txt",
+  "postal_code",
+  "purpose",
+  "confident_in_mac_ability",
+  "deadline_comms",
+  "guidelines_comms",
+  "manipogo",
+  "trasparent_granting",
+  "response_enquiries",
+  "response_results",
+  "feedback",
+  "complaints_process",
+  "improve",
+  "connection_to_arts_community",
+  "disconnect_explain_txt",
+  "rank_advocacy",
+  "rank_measure_impact",
+  "rank_board_trg",
+  "rank_financial_trg",
+  "rank_leadership_trg",
+  "rank_shared_services",
+  "rank_loan_access",
+  "rank_networking",
+  "rank_marketing",
+  "future_impact_txt",
+  "comments_txt"
+))
 
 # rename columns
 names(survey_df) <- col_names
@@ -65,7 +63,9 @@ names(survey_df) <- col_names
 # Clean and export survey -------------------------------------------------
 
 survey_df <- survey_df |>
-  select(-starts_with("x")) |> 
+  select(-starts_with("x")) |>
   mutate(date_created = date(date_created))
 
 write_rds(survey_df, "data/survey_data_clean.rds")
+
+write_csv(survey_df, "data/survey_data_clean.csv")
--- scripts/map.R
+++ scripts/map.R
@@ -5,45 +5,46 @@ library(tidyverse)
 library(rnaturalearth)
 
 # get manitoba boundary shape
-mb_boundary_map <- ne_states(country = "canada",
-                             returnclass = "sf") |> 
-  filter(name == "Manitoba") |> 
-  st_transform(crs = 4326) |> 
-  st_make_valid() |> 
+mb_boundary_map <- ne_states(country = "canada", returnclass = "sf") |>
+  filter(name == "Manitoba") |>
+  st_transform(crs = 4326) |>
+  st_make_valid() |>
   st_union()
 
 # get mb economic regions shapefiles
-mb_econ_regions_sf <- read_sf("mb_economic-regions-shape-files") |> 
-  clean_names() |> 
+mb_econ_regions_sf <- read_sf("mb_economic-regions-shape-files") |>
+  clean_names() |>
   st_transform(crs = 4326)
 
-mb_econ_regions_sf <- mb_econ_regions_sf |> 
-  mutate(economic_r = case_when(
-    economic_r == "Winnipeg" ~ "Capital Region",
-    economic_r == "Southwest" ~ "Westman",
-    economic_r == "Southeast" ~ "Eastman",
-    economic_r == "South Central" ~ "Pembina Valley",
-    economic_r == "Parkland" ~ "Parklands",
-    economic_r == "North Central" ~ "Central Plains",
-    economic_r == "North" ~ "Norman",
-    economic_r == "Interlake" ~ "Interlake"
-  ))
+mb_econ_regions_sf <- mb_econ_regions_sf |>
+  mutate(
+    economic_r = case_when(
+      economic_r == "Winnipeg" ~ "Capital Region",
+      economic_r == "Southwest" ~ "Westman",
+      economic_r == "Southeast" ~ "Eastman",
+      economic_r == "South Central" ~ "Pembina Valley",
+      economic_r == "Parkland" ~ "Parklands",
+      economic_r == "North Central" ~ "Central Plains",
+      economic_r == "North" ~ "Norman",
+      economic_r == "Interlake" ~ "Interlake"
+    )
+  )
 
 # import regions from survey
 
 data <- read_rds("data/survey_data_clean.rds")
-regions <- read_csv("data/mb-region-fsa-table.csv") |> 
+regions <- read_csv("data/mb-region-fsa-table.csv") |>
   select(-dup)
 
 
-
 mac_fsa_data <- data |>
-  select(id, postal_code) |> 
-  mutate(postal_code = str_replace_all(postal_code, " ", ""),
-         postal_code = str_to_upper(postal_code),
-         fsa = str_sub(postal_code, start = 1, end = 3)) |> 
-    filter(!is.na(fsa),
-           str_starts(fsa, "R")) |> 
+  select(id, postal_code) |>
+  mutate(
+    postal_code = str_replace_all(postal_code, " ", ""),
+    postal_code = str_to_upper(postal_code),
+    fsa = str_sub(postal_code, start = 1, end = 3)
+  ) |>
+  filter(!is.na(fsa), str_starts(fsa, "R")) |>
   select(id, fsa)
 
 
@@ -51,35 +52,28 @@ mac_fsa_data <- data |>
 #randomly assign regions with multiple occurences of FSA
 
 mac_fsa_data_linked <- mac_fsa_data |>
-  mutate(.row_id = row_number()) |> 
-  left_join(regions, by = "fsa", relationship = "many-to-many") |> 
-  group_by(.row_id) |> 
+  mutate(.row_id = row_number()) |>
+  left_join(regions, by = "fsa", relationship = "many-to-many") |>
+  group_by(.row_id) |>
   slice_sample(n = 1)
 
-mac_fsa_data_grouped <- mac_fsa_data_linked |> 
-  group_by(region) |> 
+mac_fsa_data_grouped <- mac_fsa_data_linked |>
+  group_by(region) |>
   count()
-  
+
 # link counts to shapefile
 
-mb_econ_regions_sf <-mb_econ_regions_sf |> 
-  left_join(mac_fsa_data_grouped,
-            by = c("economic_r" = "region"))
-
+mb_econ_regions_sf <- mb_econ_regions_sf |>
+  left_join(mac_fsa_data_grouped, by = c("economic_r" = "region"))
 
 
 ggplot() +
-  geom_sf(data = mb_boundary_map,
-          fill = "grey98",
-          colour = "grey50",
-          linewidth = 0.4) +
-  geom_sf(data = mb_econ_regions_sf,
-          (aes(fill = n)),
-                          colour = NA) +
+  geom_sf(
+    data = mb_boundary_map,
+    fill = "grey98",
+    colour = "grey50",
+    linewidth = 0.4
+  ) +
+  geom_sf(data = mb_econ_regions_sf, (aes(fill = n)), colour = NA) +
   scale_fill_gradient(trans = "reverse") +
   theme_void()
-
-
-
-
-
